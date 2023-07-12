ebd5cfd4c0169a25ead4eca43f98dff37f8c2ca3
chenmeng <t.chenmeng@sino-commercial.com>
2023-07-12 15:12:15 +0800

    feat: 修改结构

--- index.ts
+++ index.ts
@@ -1,8 +1,6 @@
 import express from 'express'
-import  auth  from './middleware/auth'
-import  limiter  from './middleware/limiter'
+import chatgptRouter from './routes/chatgpt'
 const app = express()
-const router = express.Router()
 
 // 静态资源
 app.use(express.static('public'))
@@ -17,74 +15,11 @@ app.all('*', (_, res, next) => {
   next()
 })
 
-
-router.post('/chat-process', [auth, limiter], async (req, res) => {
-  res.setHeader('Content-type', 'application/octet-stream')
-
-  try {
-    const { prompt, options = {}, systemMessage, temperature, top_p } = req.body as RequestProps
-    let firstChunk = true
-    await chatReplyProcess({
-      message: prompt,
-      lastContext: options,
-      process: (chat: ChatMessage) => {
-        res.write(firstChunk ? JSON.stringify(chat) : `\n${JSON.stringify(chat)}`)
-        firstChunk = false
-      },
-      systemMessage,
-      temperature,
-      top_p,
-    })
-  }
-  catch (error) {
-    res.write(JSON.stringify(error))
-  }
-  finally {
-    res.end()
-  }
-})
-
-router.post('/config', auth, async (req, res) => {
-  try {
-    const response = await chatConfig()
-    res.send(response)
-  }
-  catch (error) {
-    res.send(error)
-  }
-})
-
-router.post('/session', async (req, res) => {
-  try {
-    const AUTH_SECRET_KEY = process.env.AUTH_SECRET_KEY
-    const hasAuth = AUTH_SECRET_KEY
-    res.send({ status: 'Success', message: '', data: { auth: hasAuth, model: currentModel() } })
-  }
-  catch (error) {
-    res.send({ status: 'Fail', message: error.message, data: null })
-  }
-})
-
-router.post('/verify', async (req, res) => {
-  try {
-    const { token } = req.body as { token: string }
-    if (!token)
-      throw new Error('Secret key is empty')
-
-    if (process.env.AUTH_SECRET_KEY !== token)
-      throw new Error('密钥无效 | Secret key is invalid')
-
-    res.send({ status: 'Success', message: 'Verify successfully', data: null })
-  }
-  catch (error) {
-    res.send({ status: 'Fail', message: error.message, data: null })
-  }
-})
-
-app.use('', router)
-app.use('/api', router)
+app.use('', chatgptRouter)
+app.use('/api', chatgptRouter)
 app.set('trust proxy', 1)
 app.get('/', (req, res) => {
     res.send('Hello World!')
-  })
+})
+
 app.listen(3002, () => globalThis.console.log('Server is running on port 3002'))
